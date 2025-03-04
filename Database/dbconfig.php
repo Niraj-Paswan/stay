9ec2fd1bf6f60c8cea415499e4fb6af29ec4b9a2
Niraj Paswan <nirajpaswan.work@gmail.com>
2025-03-04 21:10:06 +0530

    included the database connection file

---
+++ Database/dbconfig.php
@@ -0,0 +1,12 @@
+<?php
+// Database connection
+$host = "localhost:3307";
+$username = "root";
+$password = "";
+$database = "stayease";
+
+$conn = new mysqli($host, $username, $password, $database);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
